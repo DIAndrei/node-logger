fb8147b5de9776551b5c80d35ce56a1bf1fb0bd5
Andrei Diaconu <andrei_di@yahoo.com>
2017-07-04 16:58:16 +0300

    Added multiple arguments for console and file logging.

--- index.js
+++ index.js
@@ -3,29 +3,40 @@
 const fs = require('fs');
 const request = require('request');
 
-
+/**
+ * 
+ */
 function ConsoleEngine() { }
 
-ConsoleEngine.prototype.write = function (timestamp, message) {
-    console.log(timestamp, message);
+
+ConsoleEngine.prototype.write = function () {
+    //console.log(arguments);
+    console.log.apply(this, arguments);
 }
 
-ConsoleEngine.prototype.error = function (timestamp, message) {
-    console.error(timestamp, message);
+
+ConsoleEngine.prototype.error = function () {
+    //console.error(arguments);
+    console.error.apply(this, arguments);
 }
 
+/**
+ * 
+ */
 function FileEngine() { }
 
-FileEngine.prototype.write = function (timestamp, message) {
-    fs.appendFile('./log/logger.log', timestamp + ' ' + message + '\r\n', function (err) {
+FileEngine.prototype.write = function () {
+    var args = Array.prototype.slice.call(arguments);
+    fs.appendFile('./log/logger.log', args + '\r\n', function (err) {
         if (err) {
             return console.log(err);
         }
     });
 }
 
-FileEngine.prototype.error = function (timestamp, message) {
-    fs.appendFile('./log/logger.error', timestamp + ' ' + message + '\r\n', function (err) {
+FileEngine.prototype.error = function () {
+    var args = Array.prototype.slice.call(arguments);
+    fs.appendFile('./log/logger.error', args + '\r\n', function (err) {
         if (err) {
             return console.log(err);
         }
@@ -35,7 +46,7 @@ FileEngine.prototype.error = function (timestamp, message) {
 /**
  * 
  */
-function HttpEngine() {}
+function HttpEngine() { }
 
 HttpEngine.prototype.sendData = function (type, timestamp, message) {
     var data = JSON.stringify({
@@ -44,7 +55,7 @@ HttpEngine.prototype.sendData = function (type, timestamp, message) {
         'timestamp': timestamp
     });
 
-    request.post('https://jsonplaceholder.typicode.com/posts', {json: true, body: data}, function(err, res, body) {
+    request.post('https://jsonplaceholder.typicode.com/posts', { json: true, body: data }, function (err, res, body) {
         //check res.statusCode
     });
 }
@@ -66,28 +77,30 @@ function Logger(logEngine) {
     this.logEngine = logEngine;
 }
 
-Logger.prototype.write = function (message) {
-    var date = new Date();
-    this.logEngine.write(date.toString(), message);
+Logger.prototype.write = function () {
+    var date = new Date().toString(),
+        args = Array.prototype.slice.call(arguments);
+
+    this.logEngine.write(date, args);
 }
 
-Logger.prototype.error = function (message) {
-    var date = new Date();
-    this.logEngine.error(date.toString(), message);
+Logger.prototype.error = function () {
+    var date = new Date().toString(),
+        args = Array.prototype.slice.call(arguments);
+
+    this.logEngine.error(date, args);
 }
-
-
 
 //----------------------------------------------------------------------
 
 var consoleEngine = new ConsoleEngine(),
     consoleLog = new Logger(consoleEngine),
     fileEngine = new FileEngine(),
-    fileLog = new Logger(fileEngine),
-    httpEngine = new HttpEngine(),
-    httpLog = new Logger(httpEngine);
+    fileLog = new Logger(fileEngine);
 
-consoleLog.write('akjdhaskjdhkasjdASDASD');
+consoleLog.write('akjdhaskjdhkasjdASDASD', 'dsfre4e', 'asdSSSSSLAPSPSPS');
 consoleLog.error('RSOIRHIHDFKLHDFGJDHFGKHJ');
-fileLog.write('teastslaksjdks');
-httpLog.write('teastslaksjdks');
+fileLog.write('teastslaksjdks', 'alsdijaosidja', 'skjdaka');
+fileLog.error('alksdadklsj', 'DKSLDKFJDLSKFj', 94872);
+
+//setTimeout(function () { consoleLog.write('asdasd', 23); }, 5000);
