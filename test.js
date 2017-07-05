a6556a73bd0012a6b5459d52911defe800c38cdb
Andrei Diaconu <andrei_di@yahoo.com>
2017-07-05 11:30:44 +0300

    Used apply method

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
 const consoleEngine = require('./lib/ConsoleEngine.js');
 const fileEngine = require('./lib/FileEngine.js');
 const httpEngine = require('./lib/HttpEngine.js');
-const logger = require('./lib/Logger.js');
+const Logger = require('./lib/Logger.js');
 
-var consoleLog = new logger(consoleEngine),
-    fileLog = new logger(fileEngine);
+var consoleLog = new Logger(consoleEngine),
+    fileLog = new Logger(fileEngine);
 
 consoleLog.write('akjdhaskjdhkasjdASDASD', 'dsfre4e', 'asdSSSSSLAPSPSPS');
 consoleLog.error('RSOIRHIHDFKLHDFGJDHFGKHJ', '39817lakjsdlkasd!@#', 22);
+
 fileLog.write('teastslaksjdks', 'alsdijaosidja', 'skjdaka');
 fileLog.error('alksdadklsj', 'DKSLDKFJDLSKFj', 94872);
 
 //setTimeout(function () { consoleLog.write('asdasd', 23); }, 5000);
 
-var httpLog = new logger(httpEngine);
+var httpLog = new Logger(httpEngine);
 
 
 httpLog.write('asdasdasd', 'AKLSDM');
